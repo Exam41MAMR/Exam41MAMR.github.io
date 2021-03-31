46a7610cb5ca5fcd753379f4a6b8dee7c6698a11
Exam41MAMR <81714423+Exam41MAMR@users.noreply.github.com>
2021-03-31 16:34:02 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,11 @@
 
 
 firebase.initializeApp({
-   apiKey: "AIzaSyBrewxCsOHOjxBwVGgX7jfptBLMGqt8psA",
+  apiKey: "AIzaSyBrewxCsOHOjxBwVGgX7jfptBLMGqt8psA",
     authDomain: "evaluacionl.firebaseapp.com",
     projectId: "evaluacionl",
     storageBucket: "evaluacionl.appspot.com",
     messagingSenderId: "572276073128",
     appId: "1:572276073128:web:ed5647447ca60e574bd89d",
     measurementId: "G-2SGZ3FEV02"
-
 });
